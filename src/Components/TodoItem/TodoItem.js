a98e4e9a9fad8437270f87ab3c65e8fa66bda3fa
Ali Helmi <ahelmi@getgroup.com>
2023-02-15 12:56:13 +0200

    add validation on todoitems cop

--- src/Components/TodoItem/TodoItem.js
+++ src/Components/TodoItem/TodoItem.js
@@ -1,37 +1,61 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./TodoItem.css";
 
 export default function TodoItem({ todoItem, setTodoItem, addTodoItem }) {
-  const todoInputRef = useRef(null);
+  const todoInputValRef = useRef(null);
+  const todoInputByRef = useRef(null);
+
+  function resetTodoInputs() {
+    todoInputValRef.current.value = "";
+    todoInputByRef.current.value = "";
+  }
+
+  const [isValidForm, setIsValidForm] = useState(false);
+  function checkFormValid() {
+    setIsValidForm(
+      Boolean(todoInputValRef.current.value && todoInputByRef.current.value)
+    );
+  }
+
   return (
     <div className="todo-item-container">
-      <form onSubmit={addTodoItem} className="todo-item-form">
+      <form
+        onSubmit={(e) => {
+          addTodoItem(e);
+          resetTodoInputs();
+          setIsValidForm(false);
+          todoInputValRef.current.focus();
+        }}
+        className="todo-item-form"
+      >
         <div className="todo-item-inputs">
           <input
             name="itemVal"
             required
             value={todoItem.itemVal}
-            onChange={setTodoItem}
+            onChange={(e) => {
+              setTodoItem(e);
+              checkFormValid(e);
+            }}
             type="text"
             placeholder="to do?"
-            ref={todoInputRef}
+            ref={todoInputValRef}
           />
           <input
             name="itemBy"
             required
             value={todoItem.itemBy}
-            onChange={setTodoItem}
+            onChange={(e) => {
+              setTodoItem(e);
+              checkFormValid(e);
+            }}
             type="text"
             placeholder="by?"
+            ref={todoInputByRef}
           />
         </div>
         <div className="submit">
-          <button
-            className="btn submit-btn "
-            onClick={() => {
-              todoInputRef.current.focus();
-            }}
-          >
+          <button disabled={!isValidForm} className="btn submit-btn ">
             Add Todo Item
           </button>
         </div>
